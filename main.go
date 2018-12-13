dda6319fa2a6d974900d90f7bd79a1bf3685651b
yasuno0327 <5315076@ed.tus.ac.jp>
2018-12-13 19:30:16 +0900

    Add migrate function

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"sandbox-api/model"
+
+	"github.com/jinzhu/gorm"
+)
 
 func main() {
+	db, err := gorm.Open("mysql", "sandbox:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	Migrate(db)
 	http.ListenAndServe(":3000", nil)
+}
+
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&model.User{})
+	db.AutoMigrate(&model.Motion{})
+	db.AutoMigrate(&model.Coupon{})
+	db.AutoMigrate(&model.Food{})
+	db.AutoMigrate(&model.DietData{})
 }
